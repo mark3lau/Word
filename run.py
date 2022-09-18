7b2719b7edf4712ad575769dba96025a65fa29a1
mark3lau <mark3lau@gmail.com>
2022-09-18 17:55:11 +0000

    Add for loop to iterate through player guess and chosen word for comparison

--- run.py
+++ run.py
@@ -21,11 +21,29 @@ def game():
     print("Start typing a five letter word related to Harry Potter, then click Enter.\n")
 
 def get_random_word():
+    """
+    Get random word from words worksheet.
+    """
     get_words = SHEET.worksheet("words")
     words = get_words.get_all_values()
-    random_word = (random.choice(words))
-    return random_word
+    return random.choice(words)
+   
 
 game()
 word = get_random_word()
 
+
+for attempt in range(1, 7):
+    guess = input().lower()
+
+    for i in range(min(len(guess), 5)):
+        if guess[i] == word[i]:
+            print(colored(guess[i], 'green'), end="")
+        elif guess[i] in word:
+            print(colored(guess[i], 'yellow'), end="")
+        else:
+            print(guess[i], end="")
+
+    if guess == word:
+        print(f"Congrats! You got the wordle in {attempt}")
+        break
