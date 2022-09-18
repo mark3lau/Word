f239f14eb1d2992073f088f2f53b0c4a484b1d56
mark3lau <mark3lau@gmail.com>
2022-09-18 16:32:32 +0000

    Connect google sheet and generate random word from sheet

--- run.py
+++ run.py
@@ -1,16 +1,31 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import random
+import sys
+from termcolor import colored
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('harry_potter_wordle')
 
 
-# import random
+def game():
+    print("Welcome to the Harry Potter Wordle Game!\n")
+    print("Start typing a five letter word related to Harry Potter, then click Enter.\n")
 
-# def game():
-#     print("Welcome to the Harry Potter Wordle Game!")
-#     print("Start typing a five letter word related to Harry Potter, then click Enter.")
+def get_random_word():
+    get_words = SHEET.worksheet("words")
+    words = get_words.get_all_values()
+    random_word = (random.choice(words))
+    return random_word
 
-# def get_random_word():
-#     words = open(words.txt)words_list.get_all_values()
-#     random_word = (random.choice(words))
-#     return random.word
+game()
+word = get_random_word()
 
-# game()
-# word = get_random_word()
-# print(word)
