694b54a9923bd8549ddc2811290eac5754ae732e
mark3lau <mark3lau@gmail.com>
2022-09-18 13:16:00 +0000

    Add welcome and instructions messages and define functions to display coloured text

--- run.py
+++ run.py
@@ -1,4 +1,24 @@
 from rich.console import Console
 from random import choice
-from words import words_list
+
+
+welcome_message = "Welcome to Wizard Wordle Game!"
+instructions = "You can start guessing with a five letter word."
+allowed_guesses = 6
+
+def correct_place(letter):
+    return f'[black on green]{letter}[/]'
+
+def correct_letter(letter):
+    return f'[black on yellow]{letter}[/]'
+
+def incorrect_letter(letter):
+    return f'[black on white]{letter}[/]'
+
+if __name__ == '__main__':
+    console = Console()
+    chosen_word = choice(word_list)
+    console.print(welcome_message)
+    console.print(instructions)
+
 
