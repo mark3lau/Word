f8e59c25c30ed8d4d6598e8460a7c3fa7f27954d
mark3lau <mark3lau@gmail.com>
2022-10-11 17:37:43 +0000

    Tidy up code syntax

--- run.py
+++ run.py
@@ -22,7 +22,8 @@ def welcome():
     instructions for the user to play the game.
     """
     termcolor.cprint("Welcome to the Harry Potter Wordle Game!\n", 'cyan')
-    termcolor.cprint("You have 6 attempts. If the letter shows up green, it's correct and in the right position. If it's red, the letter is in the word but not in the right position. If it shows up as a dash, the letter is not in the word.\n", 'cyan')
+    termcolor.cprint("You have 6 attempts. If the letter shows up green, it's correct and in the right position.", 'cyan') 
+    termcolor.cprint("If it's red, the letter is in the word but not in the right position. If it shows up as a dash, the letter is not in the word.\n", 'cyan')
     termcolor.cprint("Think of a five letter word related to Harry Potter...\n", 'cyan')
 
 
@@ -30,7 +31,7 @@ def get_random_word():
     """
     Get a random word from words worksheet.
     """
-    get_words = SHEET.worksheet("words")
+    get_words = SHEET.worksheet('words')
     words = get_words.get_all_values()
     return random.choice(words)
     
@@ -42,7 +43,7 @@ def check_guess(the_answer, the_guess):
     Display different colours to indicate right 
     and wrong answers.
     """
-    clue = ""
+    clue = ''
 
     for index, value in enumerate(the_guess):
         if (value.lower() == the_answer[index].lower()):
@@ -50,7 +51,7 @@ def check_guess(the_answer, the_guess):
         elif (value.lower() in the_answer.lower()):
             clue += colored(value.lower(), 'red')
         else:
-            clue += "-"
+            clue += '-'
     print(clue)
     return the_guess == the_answer.lower()
 
@@ -82,16 +83,16 @@ def play_game():
         guessed_correctly = check_guess(answer, guess)
   
     if guessed_correctly:
-        print(f"Congrats! You got the wordle in {attempt} guesses!")
+        print(f'Congrats! You got the wordle in {attempt} guesses!')
     else:
-        print(f"You have used up all your guesses...the correct word is {answer}")
+        print(f'You have used up all your guesses...the correct word is {answer}')
 
     play_again = input("Press Enter to play again! Or type 'mischief managed' to exit...").lower()
 
-    if play_again != "mischief managed":
+    if play_again != 'mischief managed':
         play_game()
     else:
-        print("Thanks for playing the Harry Potter Wordle game.")
+        print('Thanks for playing the Harry Potter Wordle game.')
 
 
 if __name__ == '__main__':
